8cddf13e0ad68b41d97921021a91f3965446152b
Isaque Dias <isaque.brain@gmail.com>
2026-04-16 19:47:35 -0300

    feat GREEN Valida tamanho minimo de senha

--- javascript-atv-01/src/service/UserService/UserService.js
+++ javascript-atv-01/src/service/UserService/UserService.js
@@ -3,6 +3,10 @@ class UserService {
         const passRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~])[^\s]{8,}$/;
         return passRegex.test(password)
     }
+
+    validatePassMinSize(password) {
+        return true
+    }
 }
 
 module.exports = UserService
